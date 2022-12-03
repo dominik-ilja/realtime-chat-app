4681adc497d1deccfe752fa826f9586b9267f617
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2022-12-02 19:03:39 -0500

    feat: Update server to separate chatrooms
    - Add object called rooms that stores the users and messages
    within a given room.
    - Update event listeners to interact with specific rooms

--- server/index.js
+++ server/index.js
@@ -6,48 +6,103 @@ const PORT = process.env.PORT || 5000;
 const httpServer = http.createServer();
 const io = new Server(httpServer, { cors: { origin: "*" } });
 
+const rooms = {
+  // "1": {
+  //   users: [],
+  //   messages: []
+  // }
+};
 let users = [];
 let messages = [];
 
-function addMessage(name, message) {
-  messages.push({
+function roomAlreadyHasName(name, room) {
+  return rooms[room].users.find((user) => user.name === name);
+}
+function createRoomIfItDoesNotExist(room) {
+  if (rooms[room] === undefined) {
+    rooms[room] = {
+      users: [],
+      messages: [],
+    };
+  }
+}
+function addMessage(name, room, message) {
+  createRoomIfItDoesNotExist(room);
+  const chatroom = rooms[room];
+
+  chatroom.messages.push({
     name,
     message,
     timestamp: new Date().toISOString(),
   });
-  io.emit("server message", messages);
+
+  io.to(room).emit("server message", chatroom.messages);
 }
 function addUser(name, room, id) {
   users.push({ name, room, id });
   io.emit("join server", users);
 }
-function removeUser(id) {
-  users = users.filter((user) => {
+function removeUser(room, id) {
+  createRoomIfItDoesNotExist(room);
+
+  const chatroom = rooms[room];
+  chatroom.users = chatroom.users.filter((user) => {
     if (user.id === id) {
-      addMessage("Admin", `${user.name} has left!`);
+      addMessage("Admin", room, `${user.name} has left!`);
     }
     return user.id !== id;
   });
 
-  io.emit("user disconnect server", users);
+  io.to(room).emit("user disconnect server", users);
 }
 
 io.on("connection", (socket) => {
-  // console.log("connection: ", socket.id);
+  console.log("connection: ", socket.id);
 
-  socket.on("client message", (name, message, callback) => {
-    addMessage(name, message);
+  socket.on("client message", (name, room, message, callback) => {
+    if (!name || !room || !message || !callback) return;
+
+    addMessage(name, room, message);
     callback();
   });
   socket.on("join", (name, room, callback) => {
     console.log("join", socket.id);
 
-    if (users.find((user) => user.name === name)) {
-      callback("Name already exists");
+    createRoomIfItDoesNotExist(room);
+
+    if (roomAlreadyHasName(name, room)) {
+      callback(`The name: ${name} is taken in room: ${room}`);
     } else {
-      addUser(name, room, socket.id);
-      addMessage("Admin", `${name} has joined!`);
+      const chatroom = rooms[room];
+      chatroom.users.push({ name, room, id: socket.id });
+      socket.join(room);
+      io.to(room).emit("join server", chatroom.users);
+      addMessage("Admin", room, `${name} has joined!`);
       callback(null);
+    }
+
+    console.dir(rooms, { depth: null });
+  });
+  socket.on("disconnecting", (reason) => {
+    // figure out the rooms that the socket is in and remove it
+    let i = 0;
+    for (const room of socket.rooms.values()) {
+      // the first value is going to be the socket's id so we skip it
+      if (i === 0) {
+        i = 1;
+        continue;
+      }
+
+      const chatroom = rooms[room];
+      chatroom.users = chatroom.users.filter((user) => {
+        if (user.id === socket.id) {
+          addMessage("Admin", room, `${user.name} has left`);
+        }
+
+        return user.id !== socket.id;
+      });
+      io.to(room).emit("user disconnect server", chatroom.users);
+      console.log(chatroom);
     }
   });
   socket.on("disconnect", (reason) => {
